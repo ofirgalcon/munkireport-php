cd3949d35e4a2112e5c7383d8d25bb55396eb7b8
bochoven <arjen@bochoven.net>
2020-02-11 22:37:30 +0100

    Update header and footer#

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -7,7 +7,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<title><?php echo conf('sitename'); ?></title>
-	<link rel="stylesheet" href="<?php echo conf('subdirectory'); ?>assets/themes/<?php echo sess_get('theme', 'Default')?>/bootstrap.min.css" id="bootstrap-stylesheet" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
 	<link rel="stylesheet" href="<?php echo conf('subdirectory'); ?>assets/nvd3/nv.d3.min.css" />
 	<link rel="stylesheet" href="<?php echo conf('subdirectory'); ?>assets/themes/<?php echo sess_get('theme', 'Default')?>/nvd3.override.css" id="nvd3-override-stylesheet" />
 	<link rel="stylesheet" href="<?php echo conf('subdirectory'); ?>assets/css/style.css" />
@@ -42,8 +43,7 @@
 			isManager = <?php echo $_SESSION['role'] == 'manager' ? 'true' : 'false'; ?>;
 	</script>
 
-	<script src="<?php echo conf('subdirectory'); ?>assets/js/jquery.js"></script>
-
+<script src="http://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <?php
 	if (isset($scripts))
 		foreach($scripts as $script): ?>
@@ -57,194 +57,198 @@
 	<?php if( isset($_SESSION['user'])):?>
 	<?php $modules = getMrModuleObj()->loadInfo(); ?>
 	<?php $dashboard = getDashboard()->loadAll();?>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+	<a class="navbar-brand" href="<?php echo url(''); ?>"><?php echo conf('sitename'); ?></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+
+	<?php $page = $GLOBALS[ 'engine' ]->get_uri_string(); ?>
+		<?php $url = 'show/dashboard/'; ?>
+		<?php if($dashboard->getCount() === 1):?>
+		<li <?php echo strpos($page, $url)===0?'class="nav-item active"':''; ?>>
+			<a class="nav-link" href="<?php echo url(); ?>">
+				<i class="fa fa-th-large"></i>
+				<span class="d-none d-lg-inline" data-i18n="nav.main.dashboard"></span>
+			</a>
+		</li>
+		<?php else:?>
+			<li class="dropdown<?php echo strpos($page, $url)===0?' active':''; ?>">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+					<i class="fa fa-th-large"></i>
+					<span class="d-none d-lg-inline" data-i18n="nav.main.dashboard_plural"></span>
+					<b class="caret"></b>
+				</a>
+				<ul class="dashboard dropdown-menu">
+
+					<?php foreach($dashboard->getDropdownData('show/dashboard', $page) as $item): ?>
+
+						<li class="<?=$item->class?>">
+							<a href="<?=$item->url?>">
+								<span class="pull-right"><?=strtoupper($item->hotkey)?></span>
+								<span class="dropdown-link-text "><?=$item->display_name?></span>
+							</a>
+						</li>
+
+					<?php endforeach; ?>
+
+				</ul>
+
+			</li>
+		<?php endif?>
+		<?php $url = 'show/report/'; ?>
+		<li class="nav-item dropdown">
+			<a href="#" class="nav-link dropdown-toggle<?php echo strpos($page, $url)===0?' active':''; ?>" data-toggle="dropdown">
+				<i class="fa fa-bar-chart-o"></i>
+				<span class="d-none d-lg-inline" data-i18n="nav.main.reports"></span>
+				<b class="caret"></b>
+			</a>
+			<div class="report dropdown-menu">
+
+				<?php foreach($modules->getDropdownData('reports', 'show/report', $page) as $item): ?>
+
+					<a class="dropdown-item <?=$item->class?>" href="<?=$item->url?>" data-i18n="<?=$item->i18n?>"></a>
+
+				<?php endforeach; ?>
+
+			</div>
+
+		</li>
+
+		<?php $url = 'show/listing/'; ?>
+		<li class="nav-item dropdown">
+			<a href="#" class="nav-link dropdown-toggle<?php echo strpos($page, $url)===0?' active':''; ?>" data-toggle="dropdown">
+				<i class="fa fa-list-alt"></i>
+				<span data-i18n="nav.main.listings"></span>
+				<b class="caret"></b>
+			</a>
+			<div class="listing dropdown-menu">
+
+				<?php foreach($modules->getDropdownData('listings', 'show/listing', $page) as $item): ?>
+
+					<a class="dropdown-item <?=$item->class?>" href="<?=$item->url?>" data-i18n="<?=$item->i18n?>"></a>
+
+				<?php endforeach; ?>
+
+			</div>
+
+		</li>
+
+		<?php if($_SESSION['role'] == 'admin'):?>
+		<?php $url = 'admin/show/'; ?>
+		<li class="nav-item dropdown">
+			<a href="#" class="nav-link dropdown-toggle<?php echo strpos($page, $url)===0?' active':''; ?>" data-toggle="dropdown">
+				<i class="fa fa-list-alt"></i>
+				<span data-i18n="nav.main.admin"></span>
+				<b class="caret"></b>
+			</a>
+			<div class="admin dropdown-menu">
+
+				<?php foreach(scandir(conf('view_path').'admin') as $list_url): ?>
+
+					<?php if( strpos($list_url, 'php')): ?>
+					<?php $page_url = $url.strtok($list_url, '.'); ?>
+
+						<a class="dropdown-item <?php echo strpos($page, $url)===0?' active':''; ?>" href="<?php echo url($url.strtok($list_url, '.')); ?>" data-i18n="nav.admin.<?php echo $name = strtok($list_url, '.'); ?>"></a>
+
+					<?php endif; ?>
+
+				<?php endforeach; ?>
+
+			</div>
+
+		</li>
+		<?php endif?>
+
+		<li>
+			<a class="nav-link" href="#" id="filter-popup" class="filter-popup">
+				<i class="fa fa-filter"></i>
+			</a>
+		</li>
+    </ul>
+	<ul class="navbar-nav ml-auto">
+		<li class="dropdown">
+			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+				<i class="fa fa-wrench"></i>
+			</a>
+			<div class="dropdown-menu dropdown-menu-right theme">
+
+				<?php foreach(scandir(PUBLIC_ROOT.'assets/themes') AS $theme): ?>
+
+					<?php if( $theme != 'fonts' && strpos($theme, '.') === false):?>
+
+					<a class="dropdown-item" data-switch="<?php echo $theme; ?>" href="#"><?php echo $theme; ?></a>
+
+					<?php endif; ?>
+
+				<?php endforeach; ?>
+
+			</div>
+		</li>
+		<li class="dropdown">
+			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+				<i class="fa fa-globe"></i>
+			</a>
+			<div class="dropdown-menu locale">
+
+					<?php foreach(scandir(PUBLIC_ROOT.'assets/locales') AS $list_url): ?>
+
+						<?php if( strpos($list_url, 'json')):?>
+
+						<?php $lang = strtok($list_url, '.'); ?>
+
+						<a class="dropdown-item" href="<?php echo url($page, false, ['setLng' => $lang]); ?>" data-i18n="nav.lang.<?php echo $lang; ?>"><?php echo $lang; ?></a>
+
+						<?php endif; ?>
+
+					<?php endforeach; ?>
+
+			</div>
+		</li>
+
+		<?php if( ! array_key_exists('auth_noauth', conf('auth'))): // Hide logout button if auth_noauth?>
+
+		<li class="dropdown">
+			<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+				<i class="fa fa-user"></i> <?php echo $_SESSION['user']; ?>
+				<b class="caret"></b>
+			</a>
+			<div class="dropdown-menu">
+					<a class="dropdown-item"  href="<?php echo url('auth/logout'); ?>">
+						<i class="fa fa-power-off"></i>
+						<span data-i18n="nav.user.logout"></span>
+					</a>
+			</div>
+		</li>
+
+		<?php endif; ?>
+
+
+	</ul>
+  </div>
+</nav>
 	
 <header class="navbar navbar-default navbar-static-top bs-docs-nav" role="banner">
 	<div class="container">
-		<div class="navbar-header">
-			<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="<?php echo url(''); ?>"><?php echo conf('sitename'); ?></a>
-		</div>
 		<nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
 			<ul class="nav navbar-nav">
-				<?php $page = $GLOBALS[ 'engine' ]->get_uri_string(); ?>
-				
-				<?php if($dashboard->getCount() === 1):?>
-				<li <?php echo $page==''?'class="active"':''; ?>>
-					<a href="<?php echo url(); ?>">
-						<i class="fa fa-th-large"></i>
-						<span class="visible-lg-inline" data-i18n="nav.main.dashboard"></span>
-					</a>
-				</li>
-				<?php else:?>
-					<?php $url = 'show/dashboard/'; ?>
-					<li class="dropdown<?php echo strpos($page, $url)===0?' active':''; ?>">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="fa fa-th-large"></i>
-							<span data-i18n="nav.main.dashboard_plural"></span>
-							<b class="caret"></b>
-						</a>
-						<ul class="dashboard dropdown-menu">
 
-							<?php foreach($dashboard->getDropdownData('show/dashboard', $page) as $item): ?>
 
-								<li class="<?=$item->class?>">
-									<a href="<?=$item->url?>">
-										<span class="pull-right"><?=strtoupper($item->hotkey)?></span>
-										<span class="dropdown-link-text "><?=$item->display_name?></span>
-									</a>
-								</li>
 
-							<?php endforeach; ?>
 
-						</ul>
 
-					</li>
-				<?php endif?>
-
-				<?php $url = 'show/reports/'; ?>
-				<li class="dropdown<?php echo strpos($page, $url)===0?' active':''; ?>">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						<i class="fa fa-bar-chart-o"></i>
-						<span data-i18n="nav.main.reports"></span>
-						<b class="caret"></b>
-					</a>
-					<ul class="report dropdown-menu">
-
-						<?php foreach($modules->getDropdownData('reports', 'show/report', $page) as $item): ?>
-
-							<li class="<?=$item->class?>">
-							<a href="<?=$item->url?>" data-i18n="<?=$item->i18n?>"></a>
-							</li>
-
-						<?php endforeach; ?>
-
-					</ul>
-
-				</li>
-
-				<?php $url = 'show/listing/'; ?>
-				<li class="dropdown<?php echo strpos($page, $url)===0?' active':''; ?>">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						<i class="fa fa-list-alt"></i>
-						<span data-i18n="nav.main.listings"></span>
-						<b class="caret"></b>
-					</a>
-					<ul class="listing dropdown-menu">
-
-						<?php foreach($modules->getDropdownData('listings', 'show/listing', $page) as $item): ?>
-
-							<li class="<?=$item->class?>">
-							<a href="<?=$item->url?>" data-i18n="<?=$item->i18n?>"></a>
-							</li>
-
-						<?php endforeach; ?>
-
-					</ul>
-
-				</li>
-
-				<?php if($_SESSION['role'] == 'admin'):?>
-				<?php $url = 'admin/show/'; ?>
-				<li class="dropdown<?php echo strpos($page, $url)===0?' active':''; ?>">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						<i class="fa fa-list-alt"></i>
-						<span data-i18n="nav.main.admin"></span>
-						<b class="caret"></b>
-					</a>
-					<ul class="admin dropdown-menu">
-
-						<?php foreach(scandir(conf('view_path').'admin') as $list_url): ?>
-
-							<?php if( strpos($list_url, 'php')): ?>
-							<?php $page_url = $url.strtok($list_url, '.'); ?>
-
-							<li<?php echo strpos($page, $page_url)===0?' class="active"':''; ?>>
-								<a href="<?php echo url($url.strtok($list_url, '.')); ?>" data-i18n="nav.admin.<?php echo $name = strtok($list_url, '.'); ?>"></a>
-							</li>
-
-							<?php endif; ?>
-
-						<?php endforeach; ?>
-
-					</ul>
-
-				</li>
-				<?php endif?>
-
-				<li>
-					<a href="#" id="filter-popup" class="filter-popup">
-						<i class="fa fa-filter"></i>
-					</a>
-				</li>
+			
 
 			</ul><!-- nav navbar-nav -->
 
 			<ul class="nav navbar-nav navbar-right">
 
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						<i class="fa fa-wrench"></i>
-					</a>
-					<ul class="dropdown-menu theme">
-
-							<?php foreach(scandir(PUBLIC_ROOT.'assets/themes') AS $theme): ?>
-
-								<?php if( $theme != 'fonts' && strpos($theme, '.') === false):?>
-
-								<li><a data-switch="<?php echo $theme; ?>" href="#"><?php echo $theme; ?></a></li>
-
-								<?php endif; ?>
-
-							<?php endforeach; ?>
-
-						</ul>
-				</li>
 
 
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						<i class="fa fa-globe"></i>
-					</a>
-					<ul class="dropdown-menu locale">
-
-							<?php foreach(scandir(PUBLIC_ROOT.'assets/locales') AS $list_url): ?>
-
-								<?php if( strpos($list_url, 'json')):?>
-
-								<?php $lang = strtok($list_url, '.'); ?>
-
-								<li><a href="<?php echo url($page, false, ['setLng' => $lang]); ?>" data-i18n="nav.lang.<?php echo $lang; ?>"><?php echo $lang; ?></a></li>
-
-								<?php endif; ?>
-
-							<?php endforeach; ?>
-
-						</ul>
-				</li>
-
-				<?php if( ! array_key_exists('auth_noauth', conf('auth'))): // Hide logout button if auth_noauth?>
-
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						<i class="fa fa-user"></i> <?php echo $_SESSION['user']; ?>
-						<b class="caret"></b>
-					</a>
-					<ul class="dropdown-menu">
-						<li>
-							<a href="<?php echo url('auth/logout'); ?>">
-								<i class="fa fa-power-off"></i>
-								<span data-i18n="nav.user.logout"></span>
-							</a>
-						</li>
-					</ul>
-				</li>
-
-				<?php endif; ?>
 
 				<?php if(conf('show_help')):?>
 				
